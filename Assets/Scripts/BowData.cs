27302012359bece69b75a349bb7da09378a351c2
sashamarialitinskiy <alexandralitinskiy@gmail.com>
2025-08-06 14:27:48 -0700

    Add WebGL build for GitHub Pages

--- Assets/Scripts/BowData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewBowData", menuName = "Data/BowData")]
-public class BowData : ScriptableObject
-{
-    public Sprite bowSprite;
-    public string bowName; // leave blank if you don't want to use it
-}
--- Assets/Scripts/FlowerData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class FlowerData : MonoBehaviour
-{
-    public GameManager.FlowerColor flowerColor;
-}
-
